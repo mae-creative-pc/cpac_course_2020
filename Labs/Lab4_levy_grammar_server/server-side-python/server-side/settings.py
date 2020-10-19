ca843483178d3317b375b5e7a1973d57e42b7dfe
Michele <mbuccoli@users.noreply.github.com>
2020-10-19 10:06:10 +0200

    Update settings.py

--- Labs/Lab4_levy_grammar_server/server-side-python/server-side/settings.py
+++ Labs/Lab4_levy_grammar_server/server-side-python/server-side/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tk3w3aa96ryu^znow=-*5v+evf2#==7p4c(n*2rk1wrh4+n-xa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
